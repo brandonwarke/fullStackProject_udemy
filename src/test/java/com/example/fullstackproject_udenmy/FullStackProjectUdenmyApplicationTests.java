5b75e23ad3274c5edf62911626135e6dd08e4fc8
brandon.warke <brandon.warke@bt.com>
2022-05-11 12:16:56 +0100

    First commit

---
+++ src/test/java/com/example/fullstackproject_udenmy/FullStackProjectUdenmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackproject_udenmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackProjectUdenmyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
