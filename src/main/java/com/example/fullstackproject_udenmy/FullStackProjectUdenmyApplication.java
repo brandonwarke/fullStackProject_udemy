d0d2f43ddf588e0cc30f0617f5890328461f7651
brandon.warke <brandon.warke@bt.com>
2022-09-15 12:25:38 +0100

    Testing commit conor

--- src/main/java/com/example/fullstackproject_udenmy/FullStackProjectUdenmyApplication.java
+++ src/main/java/com/example/fullstackproject_udenmy/FullStackProjectUdenmyApplication.java
@@ -8,6 +8,7 @@ public class FullStackProjectUdenmyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FullStackProjectUdenmyApplication.class, args);
+        System.out.println("Testing branch")
     }
 
 }
